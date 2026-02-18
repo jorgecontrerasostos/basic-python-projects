80a477a71301bbc0bda93a2e3bd5609450755770
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-02-18 14:29:30 -0600

    fix: fix input validation in number guessing game

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -5,16 +5,15 @@ def main():
     num = 0
 
     while num != number_to_guess:
-
-        num = int(input("Enter a number from one to 10\n"))
-        if not isinstance(num, int):
+        try:
+            num = int(input("Enter a number from one to 10\n"))
+            if num > number_to_guess:
+                print("Too high")
+            elif num < number_to_guess:
+                print("Too Low")
+            else:
+                print("Correct")
+        except ValueError:
             print("Not a number")
-        if num > number_to_guess:
-            print("Too high")
-        elif num < number_to_guess:
-            print("Too Low")
-        else:
-            print("Correct")
-
 if __name__ == "__main__":
     main()
