b551fdf82e594fa84b200e471d01d47e3b41e680
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-02-18 18:16:01 -0600

    style: code formatting

--- unit-converter/main.py
+++ unit-converter/main.py
@@ -1,11 +1,12 @@
 import logging
+from rich import print
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 logger = logging.getLogger(__name__)
+
 
 def convert_f_to_c(num: float) -> float:
     """
@@ -15,6 +16,7 @@ def convert_f_to_c(num: float) -> float:
     """
     return (num - 32) / 1.8
 
+
 def convert_c_to_f(num: float) -> float:
     """
     TODO: Add docstrings
@@ -23,7 +25,8 @@ def convert_c_to_f(num: float) -> float:
     """
     return (num * 1.8) + 32
 
-def convert_m_to_km(num: float) -> float:
+
+def convert_miles_to_km(num: float) -> float:
     """
     TODO: Add docstrings
     :param num:
@@ -31,13 +34,16 @@ def convert_m_to_km(num: float) -> float:
     """
     return num * 1.60934
 
-def convert_km_to_m(num: float) -> float:
+
+def convert_km_to_miles(num: float) -> float:
     """
     TODO: Add docstrings
     :param num:
     :return:
     """
     return num / 1.60934
+
+
 def convert_lbs_to_kg(num: float) -> float:
     """
     TODO: Add docstrings
@@ -46,6 +52,7 @@ def convert_lbs_to_kg(num: float) -> float:
     """
     return num * 0.453592
 
+
 def convert_kg_to_lbs(num: float) -> float:
     """
     TODO: Add docstrings
@@ -53,6 +60,7 @@ def convert_kg_to_lbs(num: float) -> float:
     :return:
     """
     return num / 0.453592
+
 
 def temperature_menu() -> None:
     """
@@ -64,15 +72,16 @@ def temperature_menu() -> None:
     print("3. Go Back")
     temp_option = input("Pick a conversion\n")
     try:
-        if temp_option == '1' or temp_option == '2':
+        if temp_option == "1" or temp_option == "2":
             num = input("Insert a number:\n")
             casted_num = float(num)
-            if temp_option == '1':
+            if temp_option == "1":
                 print(convert_c_to_f(casted_num))
-            elif temp_option == '2':
+            elif temp_option == "2":
                 print(convert_f_to_c(casted_num))
     except ValueError:
         logger.error(f"{num} is not a number")
+
 
 def distance_menu() -> None:
     """
@@ -84,15 +93,16 @@ def distance_menu() -> None:
     print("3. Go Back")
     dist_option = input("Pick a conversion\n")
     try:
-        if dist_option == '1' or dist_option == '2':
+        if dist_option == "1" or dist_option == "2":
             num = input("Insert a number:\n")
             casted_num = float(num)
-            if dist_option == '1':
-                print(convert_m_to_km(casted_num))
-            elif dist_option == '2':
-                print(convert_km_to_m(casted_num))
+            if dist_option == "1":
+                print(convert_miles_to_km(casted_num))
+            elif dist_option == "2":
+                print(convert_km_to_miles(casted_num))
     except ValueError:
         logger.error(f"{num} is not a number")
+
 
 def weight_menu() -> None:
     print("1. LBS TO KG")
@@ -100,15 +110,16 @@ def weight_menu() -> None:
     print("3. Go Back")
     weight_option = input("Pick a conversion:\n")
     try:
-        if weight_option == '1' or weight_option == '2':
+        if weight_option == "1" or weight_option == "2":
             num = input("Insert a number:\n")
             casted_num = float(num)
-            if weight_option == '1':
+            if weight_option == "1":
                 print(convert_lbs_to_kg(casted_num))
-            elif weight_option == '2':
+            elif weight_option == "2":
                 print(convert_kg_to_lbs(casted_num))
     except ValueError:
         logger.error(f"{num} is not a number")
+
 
 def main():
     while True:
@@ -120,20 +131,21 @@ def main():
 
         option = input("Select an option\n")
 
-        if option == '1':
+        if option == "1":
             temperature_menu()
 
-        elif option == '2':
+        elif option == "2":
             distance_menu()
 
-        elif option == '3':
+        elif option == "3":
             weight_menu()
 
-        elif option == 'q' or option == 'Q':
+        elif option == "q" or option == "Q":
             break
 
         else:
             print("Option not found")
+
 
 if __name__ == "__main__":
     main()
